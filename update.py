1f53f09ba9b13dc9b7ce4ae15a2ba5e5eb34a0e8
Carl Schader <carlschader@gmail.com>
2021-08-20 16:16:46 -0600

    v1.0.10

--- update.py
+++ update.py
@@ -2,13 +2,26 @@
 
 import sys
 import os
+import re
 
 version = sys.argv[1]
+homebrewDir = "/usr/local/Homebrew"
+
+binaryUrl = "https://github.com/KeySpot/keyspot/releases/download/v{0}/keyspot_{0}_Darwin_x86_64.tar.gz".format(version)
+
+os.system("brew create {}".format(binaryUrl))
+
+formulaPath = os.path.join(homebrewDir, "Library", "Taps", "homebrew", "homebrew-core", "Formula", "keyspot.rb")
+
+formulaFile = open(formulaPath, 'r')
+checksum = re.search('sha256 (.+?)\n', formulaFile.read()).group(1)
+formulaFile.close()
+
 
 formula = """class Keyspot < Formula
   desc "KeySpot CLI tool for accessing records and injecting variables into an environment without needing .env files"
   homepage "https://keyspot.app"
-  url "https://github.com/KeySpot/keyspot/releases/download/v{0}/keyspot_{0}_Darwin_x86_64.tar.gz"
+  url "{}"
   sha256 "db4a181befa34333f21338ff45db85a1f969585572f9d018ec5c57a35fe6bb62"
   license "MIT"
 
@@ -16,7 +29,7 @@ formula = """class Keyspot < Formula
     bin.install 'keyspot'
   end
 end
-""".format(version)
+""".format(binaryUrl)
 
 formulaFile = open("keyspot.rb", 'w')
 formulaFile.write(formula)
