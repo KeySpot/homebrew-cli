41027980c926141461a1efc7d5460abdd8311024
carlschader <carlschader@gmail.com>
2021-08-23 09:20:26 -0600

    v1.0.22

--- update.sh
+++ update.sh
@@ -4,11 +4,7 @@ version=$(git ls-remote --tags https://github.com/keyspot/cli-tool.git | tail -n
 
 binaryUrl="https://github.com/KeySpot/cli-tool/releases/download/$version/cli-tool_${version:1}_Darwin_x86_64.tar.gz"
 
-curl $binaryUrl --output binary.tar.gz
-
-checksum=$(curl -Ls $binaryUrl | shasum -a 256 | awk '{print $1;}')
-
-echo $checksum
+checksum=$(curl -Ls $binaryUrl | shasum -a 256 | awk '{print $1;}')      
 
 formula="class Keyspot < Formula\n\tdesc \"KeySpot CLI tool for accessing records and injecting variables into an environment without needing .env files\"\n\thomepage \"https://keyspot.app\"\n\turl \"${binaryUrl}\"\n\tsha256 \"${checksum}\"\n\tlicense \"MIT\"\n\n\tdef install\n\t\tbin.install 'cli-tool' => 'keyspot'\n\tend\nend\n"
 
