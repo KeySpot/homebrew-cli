84b3b45b4182273cafb9a0b8cbe9d899b668b214
carlschader <carlschader@gmail.com>
2021-09-11 00:04:13 -0600

    v1.1.5

--- keyspot.rb
+++ keyspot.rb
@@ -1,8 +1,8 @@
 class Keyspot < Formula
 	desc "KeySpot CLI tool for accessing records and injecting variables into an environment without needing .env files"
 	homepage "https://keyspot.app"
-	url "https://github.com/KeySpot/cli-tool/releases/download/v1.1.4/cli-tool_1.1.4_Darwin_x86_64.tar.gz"
-	sha256 "f80cd9e806bd168f0c8217eb564e725a6d30bbc3ca65707ac9a0fa9919f9858d"
+	url "https://github.com/KeySpot/cli-tool/releases/download/v1.1.5/cli-tool_1.1.5_Darwin_x86_64.tar.gz"
+	sha256 "5ef0b8508701abf01ac6e8fa2d6fe2ada70a978e90baa279055e319e89696769"
 	license "MIT"
 
 	def install
